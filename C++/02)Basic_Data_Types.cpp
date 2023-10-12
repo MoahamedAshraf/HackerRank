8f914073f197f6367ea21d7a914975c279ec130e
Jimmy <mohamedashraf7217@gmail.com>
2023-10-12 15:46:25 +0300

    update

---
+++ C++/02)Basic_Data_Types.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    char x;
+    int y;
+    long z;
+    float c;
+    double v;
+    
+   scanf("%d %ld %c %f %lf ",&y,&z,&x,&c,&v);
+   printf("%d\n%ld\n%c\n%f\n%lf\n",y,z,x,c,v);
+    return 0;
+}
