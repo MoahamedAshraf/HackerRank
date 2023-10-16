774ad1808ce623539f4db80db1ed6fa7abbd4737
Jimmy <mohamedashraf7217@gmail.com>
2023-10-16 22:46:55 +0300

    update

--- Problem-Solving/11)Sum_Vs_XOR.c
+++ Problem-Solving/11)Sum_Vs_XOR.c
@@ -9,12 +9,18 @@
 #include <stdlib.h>
 #include <string.h>
 
-/*
- * Complete the 'sumXor' function below.
- *
- * The function is expected to return a LONG_INTEGER.
- * The function accepts LONG_INTEGER n as parameter.
- */
+ /**
+  * we know that (n+i)=(n^i)+2*(n&i)
+  * So n + i = n ^ i implies n & i = 0
+  * We can use the count of unset-bits in the binary representation of n. 
+  * If the kth bit is set at a particular in n, kth bit in i must be 0 always, else kth bit of i can be 0 or 1
+  * Hence, total such combinations are 2^(count of unset bits in n)
+  * For example, consider n = 12 (Binary representation : 1 1 0 0). 
+  * All possible values of i that can unset all bits of n are 0 0 0/1 0/1
+  * (0/1) implies either 0 or 1. Number of such values of i are 2^2 = 4. 
+  * 
+  * https://www.geeksforgeeks.org/equal-sum-xor/
+  */
 
 long sumXor(long n) 
 {
@@ -25,11 +31,13 @@ long sumXor(long n)
         if((n&1) == 0)
         {
             count<<=1;
+            // or count++; while count is initialized with zero
         }
         n = n>>1;
     }
     
     return count;
+    //or return 1<<count; which implies 2^(count of zeros).
 }
 
 int main()
